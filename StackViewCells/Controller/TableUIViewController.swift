aa66d94bcbf71d151c220c8e7827eb1f0a466460
Benjamin Inemugha <binemugha@gmail.com>
2021-02-22 07:37:37 +0100

    Added an array of dictionary

--- StackViewCells/Controller/TableUIViewController.swift
+++ StackViewCells/Controller/TableUIViewController.swift
@@ -12,9 +12,11 @@ class TableUIViewController: UIViewController, UITableViewDelegate, UITableViewD
     
 
     @IBOutlet weak var tableView: UITableView!
-    
+    //Change the table view data to match either of data, name or bigdata
     var data = ["Drugs", "Money"]
-    var name = [["Paracetamol", "Dollars","Queens", ["Boys", "Men", "Children"]], ["Paracetamol1", "Dollars1","Queens1", ["Boys1", "Men1", "Children1"]]]
+    var name = [["Paracetamol", "Dollars","Queens", ["Boys", "Men", "Children"]], ["Paracetamol1", "Dollars1","Queens1", ["Boys1", "Men1", "Children1"]], ["Paracetamol", "Dollars","Queens", ["Boys", "Men", "Children"]]]
+    var bigdata = [(id: 1, name: "Emzor Pharmaceutical", email: "info@emzorpharma.com", phone: "+2347080606000", address: "Plot 3C, Block A, Aswani Market", city: "Isolo", state: "Lagos", country: "NG", longitude: "3.321724", latitude: "6.529160", pharmacy_items: [(id: 323, order: 204, product_name: "Paramax", product: 3, price: "500.0000", quantity: 1, fulfillment_status: "awaiting_pickup", get_fulfillment_status_display: "Awaiting Pickup", strength: "500", strength_measurement_unit: "mg", dosage_route: "oral/caplet")])]
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
@@ -25,13 +27,23 @@ class TableUIViewController: UIViewController, UITableViewDelegate, UITableViewD
     }
     
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return name.count
+        //Uncomment the array you want to use
+        
+        //return data.count
+        //return name.count
+        return bigdata.count
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         if let cell = tableView.dequeueReusableCell(withIdentifier: "TableViewCell", for: indexPath) as? TableViewCell {
-            print("\(#function) --- section = \(indexPath.section), row = \(indexPath.row)")
-            cell.tableViewLbl.text = name[indexPath.row][2] as? String
+//            print("\(#function) --- section = \(indexPath.section), row = \(indexPath.row)")
+            
+            
+            //Uncomment the arrays you want to use
+            
+            //cell.tableViewLbl.text = data[indexPath.row]
+            //cell.tableViewLbl.text = name[indexPath.row][1] as? String
+            cell.tableViewLbl.text = bigdata[indexPath.section].name
             return cell
         }
         return UITableViewCell()
